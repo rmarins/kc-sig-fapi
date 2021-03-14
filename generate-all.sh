ee0148815dc5eaa232c6fc5120e12950a1ad934d
Rafael Marins <rafa.marins@gmail.com>
2021-03-14 09:21:03 -0300

    Take CLIENT_JWKS_URL param as the 5th argument

--- generate-all.sh
+++ generate-all.sh
@@ -4,14 +4,16 @@
 # ARG2: Hostname of FAPI Conformance suite server
 # ARG3: Hostname of Keycloak server
 # ARG4: Hostname of Resource server
-# ARG5: Realm name
-# ARG6: Scope
+# ARG5: HTTP server providing client JWKS (urls)
+# ARG6: Realm name
+# ARG7: Scope
 FCSS_ALIAS=${1:-keycloak}
 FCSS_HOST=${2:-conformance-suite.keycloak-fapi.org}
 KC_HOST=${3:-as.keycloak-fapi.org}
 RS_HOST=${4:-rs.keycloak-fapi.org}
-REALM=${5:-test}
-SCOPE=${6:-openid}
+CLIENT_JWKS_URL=${5:-http://client_jwks_server:3000/}
+REALM=${6:-test}
+SCOPE=${7:-openid}
 
 DIR=$(cd $(dirname $0); pwd)
 cd $DIR
@@ -19,5 +21,6 @@ cd $DIR
 ./https/generate-server.sh $KC_HOST $RS_HOST
 ./https/generate-clients.sh
 ./client_private_keys/generate-keys.sh
-./keycloak/generate-realm.sh $FCSS_HOST $FCSS_ALIAS $REALM
+./keycloak/generate-realm.sh $FCSS_HOST $FCSS_ALIAS $REALM $CLIENT_JWKS_URL
 ./fapi-conformance-suite-configs/generate-fapi-conformance-suite-configs.sh $KC_HOST $RS_HOST $FCSS_ALIAS $REALM $SCOPE
+
